d6c71bbe7da84cb6000295ef26e049c8e449436c
Jake Furlong <jake.w.furlong@gmail.com>
2025-04-18 12:16:37 -0700

    removed gist dir:

--- gist-stuff/test-call.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-response = client.responses.create(
-    model="gpt-4o",
-    input="Write a humorous devops haiku."
-)
-
-print(response.output_text)
